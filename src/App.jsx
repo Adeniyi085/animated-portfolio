df5971aaad8556f0e0eb2803b5d7e4357faf61c9
Oluwatobi Adegelu <adeniyi.adegelu@gmail.com>
2024-02-01 10:24:55 +0100

    Navbar done and  Sidebar working with Framer-motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+
+import './app.scss';
+import Navbar from './components/navbar/Navbar';
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+<section>
+  <Navbar />
+</section>
+<section>PARALLAX</section>
+<section>SERVICES</section>
+<section>PARALLAX</section>
+<section>PORTFOLIO 1</section>
+<section>PORTFOLIO 2</section>
+<section>PORTFOLIO 3</section>
+<section>CONTACT</section>
+  </div>;
 };
 
 export default App;
